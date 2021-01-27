adulau/ssldump

commit 64effa3bb93c3a219fb0afd868c5bc2609093ced

Clean remaining json object in case of error

ssl/sslxprint.c
@@ -73,6 +73,7 @@ int sslx_print_certificate(ssl,data,pf)
 #endif    
     UCHAR *d;
     int _status;
+    struct json_object *cert_obj;
     
 #ifdef OPENSSL        
     P_(P_ASN){
@@ -86,7 +87,6 @@ int sslx_print_certificate(ssl,data,pf)
       struct json_object *jobj;
       jobj = ssl->cur_json_st;
 
-      struct json_object *cert_obj;
       cert_obj = json_object_new_object();
 
       d=data->data;
@@ -183,7 +183,8 @@ int sslx_print_certificate(ssl,data,pf)
   abort:
 #ifdef OPENSSL    
     if(x) X509_free(x);
-#endif    
+#endif
+    if(cert_obj) json_object_put(cert_obj);
     return(_status);
   }  
 

commit 5e7d876af25fd85fd90696425602623998c867ec

Fix bug introduced in 64effa3bb93c3a219fb0afd868c5bc2609093ced

ssl/sslxprint.c
@@ -184,7 +184,7 @@ int sslx_print_certificate(ssl,data,pf)
 #ifdef OPENSSL    
     if(x) X509_free(x);
 #endif
-    if(cert_obj) json_object_put(cert_obj);
+    if(_status && cert_obj) json_object_put(cert_obj);
     return(_status);
   }  
 